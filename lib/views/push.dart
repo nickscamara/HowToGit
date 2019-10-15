dd4fa424a60d768f78e9d332156853e975f6c535
Nicolas <nicolascamara29@gmail.com>
2019-10-15 16:14:33 -0400

    ok

--- lib/views/push.dart
+++ lib/views/push.dart
@@ -69,7 +69,32 @@ class PushScreen extends StatelessWidget {
                         ),
                       ),
                       
-                      Container(child: Image.asset("assets/img/mynewfolder.png"),),
+                      Container(child: Image.asset("assets/img/pull2.png"),),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Now that you made changes, it is time to push all this information to the project. For that, you need to commit to your workspace by adding a message in the summary box below.',
+                          style: TextStyle(
+                            height: 1.2,
+                            color: Colors.black,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                     
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Click the blue button that says 'Commit to Master'. This is means that you are appending the code to your project.",
+                          style: TextStyle(
+                            height: 1.2,
+                            color: Colors.black,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
                      
                       
                       
